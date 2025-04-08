cda35eba369764398a20637c80ed77d38023dcf3
Zhen Wang <wangz1996@sjtu.edu.cn>
2025-04-08 15:23:23 +0800

    Temporary solution for electronical tests

--- src/WaveReader.cc
+++ src/WaveReader.cc
@@ -37,7 +37,7 @@ bool WaveReader::readWave(){
 	unsigned char b1 = static_cast<unsigned char>(rest[6660]);
 	unsigned char b2 = static_cast<unsigned char>(rest[6661]);
 	if(b1==0x5a && b2==0xa5){
-		for(size_t chn_i=0;chn_i<26;chn_i++){
+		for(size_t chn_i=0;chn_i<NChn;chn_i++){
 			TString name = TString::Format("chn%d",chn_i+1);
 			for(size_t byte_i=0;byte_i<128;byte_i++){
 				int init_pos = 256*(chn_i) + byte_i*2;
@@ -60,26 +60,47 @@ bool WaveReader::readWave(){
 
 bool WaveReader::readAmp(){
 	//124 Bytes/Package
-	std::memset(chn,0,sizeof(chn));
-	std::memset(chnplat,0,sizeof(chnplat));
+	if(EventCount==0){
+		CellID = std::vector<int>(0, 0);
+		CellADC = std::vector<int>(0, 0);
+		CellPLAT = std::vector<int>(0, 0);
+	}
+
 	char btime[10];
 	file->rdbuf()->sgetn(btime,sizeof(btime));
+	unsigned char b7 = static_cast<unsigned char>(btime[7]);
+	unsigned int FEEID = static_cast<int>((b7 & 0xF0) >> 4) - 4; //FEEID
 	char rest[110];
 	file->rdbuf()->sgetn(rest,sizeof(rest));
+	// unsigned char trigger_id = static_cast<unsigned char>(btime[104]);
+	// unsigned char trigger_id2 = static_cast<unsigned char>(btime[105]);
+	//concat lower part of id with all id2
+	// unsigned int triggerid = static_cast<int>((trigger_id & 0x0F) << 8 | trigger_id2);
+	//Print trigger id in binary format
+	// std::cout<<"triggerid: "<<triggerid<<" "<<std::bitset<16>(triggerid)<<std::endl;
 	unsigned char b1 = static_cast<unsigned char>(rest[108]);
 	unsigned char b2 = static_cast<unsigned char>(rest[109]);
 	if(b1==0x5a && b2==0xa5){
-		for(size_t chn_i=0;chn_i<26;chn_i++){
+		for(size_t chn_i=0;chn_i<NChn;chn_i++){
 			auto d1 = static_cast<unsigned char>(rest[chn_i*4]);
 			auto d2 = static_cast<unsigned char>(rest[chn_i*4+1]);
 			auto d3 = static_cast<unsigned char>(rest[chn_i*4+2]);
 			auto d4 = static_cast<unsigned char>(rest[chn_i*4+3]);
 			unsigned short plat = (static_cast<unsigned short>(d1)<<8) | d2;
 			unsigned short maxi = (static_cast<unsigned short>(d3)<<8) | d4;
-            chnplat[chn_i] = plat;
-            chn[chn_i] = maxi - plat;
+			int tmp_cellid = 10000 * FEEID + 1000 * ( FEEID % 2 ) + 100 * (channelMap[std::pair<int,int>(FEEID,chn_i)].first) + chn_i;
+			CellID.emplace_back(tmp_cellid);
+			CellADC.emplace_back(static_cast<int>(maxi));
+            CellPLAT.emplace_back(plat);
 		}
-		tout->Fill();
+		if(EventCount==1){
+			tout->Fill();
+			EventID++;
+			EventCount=0;
+		}
+		else{
+			EventCount++;
+		}
 		return true;
 	}
 	return false;
@@ -89,20 +110,19 @@ void WaveReader::decode(const std::string& filename){
     getOutputName(filename);
     fout = new TFile(oname.c_str(),"RECREATE");
     tout = new TTree("tree","tree");
-    for(int i=0;i<NChn;i++){
-        tout->Branch(TString::Format("chn%d",i+1),&chn[i]);
-        tout->Branch(TString::Format("chnplat%d",i+1),&chnplat[i]);
-    }
+	tout->Branch("EventID",&EventID);
+	tout->Branch("CellID",&CellID);
+	tout->Branch("CellADC",&CellADC);
+	tout->Branch("CellPLAT",&CellPLAT);
     openFile(filename);
     while(!file->eof()){
-		if(npackages>170000)break;
         if(findHead()){
+			npackages++;
             if(mode == WorkMode::WAVE){
                 readWave();
             }
             else if(mode == WorkMode::AMP){
                 readAmp();
-				npackages++;
             }
             else{
                 continue;
@@ -110,6 +130,7 @@ void WaveReader::decode(const std::string& filename){
         }
         else{break;}
     }
+	std::cout<<"Total packages: "<<npackages<<std::endl;
     fout->cd();
     tout->Write();
     fout->Close();
